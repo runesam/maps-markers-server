e5508844abf3eef1c017aaf4c398204892248100
runesam <same7.hamada@gmail.com>
2019-07-14 00:24:16 +0200

    [feature] allow cross origin calls, add [user GET, markers POST, PUT and DELETE]

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,25 @@
 // @flow
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import { findAddress } from '../module/geo.module';
+import markersModule from '../module/markers.module';
 
 export const app = express();
-const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
+declare var process: { env: { port: number, key: string } };
+const { port, key } = process.env;
+
+markersModule.initialMarkersJSON();
+
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
+
+app.use(bodyParser.json());
 
 app.get('/geo/:address', (req, res) => {
     const { address } = req.params;
@@ -30,5 +38,38 @@ app.get('/geo/:address', (req, res) => {
         });
 });
 
+app.get('/user', (req, res) => {
+    const markers = markersModule.getMarkers();
+    console.log(markers);
+    res.status(200);
+    res.send({ key, markers });
+});
+
+app.post('/markers', (req, res) => {
+    const result = markersModule.addMarker(req.body);
+    if (result.error) {
+        res.status(400);
+        return res.send(result);
+    }
+    res.status(201);
+    return res.send(result);
+});
+
+app.put('/markers', (req, res) => {
+    const result = markersModule.updateMarker(req.body);
+    if (result.error) {
+        res.status(400);
+        return res.send(result);
+    }
+    res.status(200);
+    return res.send(result);
+});
+
+app.delete('/markers/:markerId', (req, res) => {
+    const { markerId } = req.params;
+    const result = markersModule.deleteMarker({ id: markerId });
+    res.status(202);
+    return res.send(result);
+});
 
 export default app.listen(port, () => console.log(`express init, listening to port ${port}`));
--- test/app/index.test.js
+++ test/app/index.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 
 import server, { app } from 'app';
 import * as geoModule from 'module/geo.module';
+import markersModule from 'module/markers.module';
 
 describe('app', () => {
     afterAll((done) => {
@@ -23,13 +24,17 @@ describe('app', () => {
             geoModule.findAddress.mockResolvedValue(response);
 
             request(app)
-                .get('/geo/grunewaldStr.')
+                .get('/geo/grunewaldStr./')
                 .expect(200)
-                .expect(res => expect(res.body).toEqual(response))
-                .end(() => {
-                    expect(spy).toHaveBeenCalledTimes(1);
-                    expect(spy).toHaveBeenCalledWith('grunewaldStr.');
-                    done();
+                .expect(res => expect(res.body).toEqual(response.data))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        expect(spy).toHaveBeenCalledWith('grunewaldStr.');
+                        done();
+                    }
                 });
         });
 
@@ -60,6 +65,170 @@ describe('app', () => {
                     expect(spy).toHaveBeenCalledTimes(1);
                     expect(spy).toHaveBeenCalledWith('grunewaldStr.');
                     done();
+                });
+        });
+    });
+
+    describe('/user endpoint', () => {
+        let spy;
+        beforeEach(() => {
+            spy = jest.spyOn(markersModule, 'getMarkers');
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('returns 200 with markers', (done) => {
+            const { key } = process.env;
+            const markers = [{ id: 3 }];
+            markersModule.getMarkers.mockReturnValue(markers);
+
+            request(app)
+                .get('/user/')
+                .expect(200)
+                .expect(res => expect(res.body).toEqual({ markers, key }))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        done();
+                    }
+                });
+        });
+    });
+
+    describe('/markers post endpoint', () => {
+        let spy;
+        beforeEach(() => {
+            spy = jest.spyOn(markersModule, 'addMarker');
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('returns 201 with added marker', (done) => {
+            const marker = { id: 5 };
+            markersModule.addMarker.mockReturnValue(marker);
+
+            request(app)
+                .post('/markers')
+                .send(marker)
+                .expect(201)
+                .expect(res => expect(res.body).toEqual(marker))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        expect(spy).toHaveBeenCalledWith(marker);
+                        done();
+                    }
+                });
+        });
+
+        it('returns 400 with error message', (done) => {
+            const marker = { id: 5 };
+            const reason = { error: 'couldn\'t add ' };
+            markersModule.addMarker.mockReturnValue(reason);
+
+            request(app)
+                .post('/markers')
+                .send(marker)
+                .expect(400)
+                .expect(res => expect(res.body).toEqual(reason))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        expect(spy).toHaveBeenCalledWith(marker);
+                        done();
+                    }
+                });
+        });
+    });
+
+    describe('/markers put endpoint', () => {
+        let spy;
+        beforeEach(() => {
+            spy = jest.spyOn(markersModule, 'updateMarker');
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('returns 200 with updated marker', (done) => {
+            const marker = { id: 5, idToReplace: 3 };
+            markersModule.updateMarker.mockReturnValue(marker);
+
+            request(app)
+                .put('/markers')
+                .send(marker)
+                .expect(200)
+                .expect(res => expect(res.body).toEqual(marker))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        expect(spy).toHaveBeenCalledWith(marker);
+                        done();
+                    }
+                });
+        });
+
+        it('returns 400 with error message', (done) => {
+            const marker = { id: 5 };
+            const reason = { error: 'couldn\'t update ' };
+            markersModule.updateMarker.mockReturnValue(reason);
+
+            request(app)
+                .put('/markers')
+                .send(marker)
+                .expect(400)
+                .expect(res => expect(res.body).toEqual(reason))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        expect(spy).toHaveBeenCalledWith(marker);
+                        done();
+                    }
+                });
+        });
+    });
+
+    describe('/markers/:markerToDelete endpoint', () => {
+        let spy;
+        beforeEach(() => {
+            spy = jest.spyOn(markersModule, 'deleteMarker');
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('returns 202 with deleted marker', (done) => {
+            const marker = { id: 5, idToReplace: 3 };
+            markersModule.deleteMarker.mockReturnValue(marker);
+
+            request(app)
+                .delete(`/markers/${marker.id}`)
+                .expect(202)
+                .expect(res => expect(res.body).toEqual(marker))
+                .end((err) => {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        expect(spy).toHaveBeenCalledTimes(1);
+                        expect(spy).toHaveBeenCalledWith({ id: '5' });
+                        done();
+                    }
                 });
         });
     });
